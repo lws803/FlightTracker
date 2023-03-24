cac62ce2557881d07f2a997b3f87e2372505ec94
lws803 <lws803@gmail.com>
2023-03-23 23:49:51 -0400

    added some comments

--- src/WordCompletion/WordCompletion.java
+++ src/WordCompletion/WordCompletion.java
@@ -10,16 +10,15 @@ import Common.Utils;
 public class WordCompletion {
   private static Scanner scanner;
 
-  public static void main(String[] args) {
-
-  }
-
   public static void run() throws Exception {
     scanner = new Scanner(System.in);
 
     String[] fileContent = null;
     TrieST<Integer> trie = new TrieST<Integer>();
     File folder = new File("src/resc/Web Pages/");
+
+    // Reads the contents of files in the "src/resc/Web Pages/" directory (one at a
+    // time)
     for (File fileEntry : folder.listFiles()) {
       fileContent = Utils.readFile(folder.getAbsolutePath() + "/" + fileEntry.getName());
       if (fileContent != null)
@@ -33,9 +32,12 @@ public class WordCompletion {
 
     System.out.println("Enter a word:");
     String word = scanner.nextLine();
+
     if (trie.contains(word)) {
+      // If the word exists in the trie, prints a blank line to the console.
       StdOut.println("");
     } else {
+      // If the word does not exist in the trie, prints "Related Words" to the console
       StdOut.println("Related Words");
       for (String s : trie.keysWithPrefix(word))
         StdOut.println(s);
