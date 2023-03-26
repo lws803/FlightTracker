75cfa09f0c715f132ed494b8b644823fb2fc3760
Darsh <darshkothari1199@gmail.com>
2023-03-25 20:53:55 -0400

    Darsh changes(InvertedIndex)

--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -10,20 +10,27 @@ import Std.StdOut;
 import Trie.TrieST;
 
 public class InvertedIndex {
+	// Initialize a Trie data structure to store the inverted index
 	static TrieST<Map<String, Integer>> wordTrie = new TrieST<>();
 
+	// Method to parse HTML content from a given URL and return a list of words
 	static List<String> htmlParse(String url) {
 		// Document doc;
 		Document doc;
 		// adding try block to check for exceptions
 		try {
+			// Connect to the URL and retrieve the HTML content
 			doc = Jsoup.connect(url).get();
 			String body = doc.body().text();
 			// adding words in an array
+
+			// Split the content into individual words and store them in a list
 			String[] str = body.split("\\s+");
 			List<String> name = new ArrayList<String>();
 			for (int i = 0; i < str.length; i++) {
 				// deleting non alphanumeric characters
+
+				// Remove any non-alphanumeric characters and convert to lowercase
 				name.add(str[i].replaceAll("[^\\w]", "").toLowerCase());
 			}
 			// System.out.println("Length of string is: "+ str.length);
@@ -32,22 +39,27 @@ public class InvertedIndex {
 			return name;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			// Print the stack trace if there is an exception
 			e.printStackTrace();
 		}
 		return null;
 	}
 
+	// Method to construct the inverted index for a given file and list of words
 	static void constructTrie(String filename, List<String> words) {
+		// Create a hashmap to store the frequency of each word in the list
 		HashMap<String, Integer> wordFreq = new HashMap<String, Integer>();
 		for (String word : words) {
 			if (!wordFreq.containsKey(word)) {
 				wordFreq.put(word, 1);
 			} else {
+				// If the word is already in the hashmap, increment its frequency
 				Integer x = wordFreq.get(word);
 				wordFreq.replace(word, x + 1);
 			}
 		}
 
+		// For each word, check if it is already in the trie. If not, add it with its frequency
 		for (String key : wordFreq.keySet()) {
 			if (!wordTrie.contains(key)) {
 				HashMap<String, Integer> rowMap = new HashMap<String, Integer>();
@@ -56,6 +68,7 @@ public class InvertedIndex {
 			}
 		}
 
+		// Print the inverted index
 		for (String key : wordTrie.keys()) {
 			StdOut.println(key + " " + wordTrie.get(key));
 		}
